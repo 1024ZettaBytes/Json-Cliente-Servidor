89c1a794ac09ba81d682a4bbcef7ddebc10634a3
Eduardo Ramírez <ramirezcota.eduardo@gmail.com>
2019-02-23 17:56:53 -0700

    Final

--- OperacionesJSON/src/client/socketCliente.java
+++ OperacionesJSON/src/client/socketCliente.java
@@ -8,6 +8,7 @@ package client;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
@@ -17,30 +18,36 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.json.JSONObject;
 
-
 /**
  *
  * @author EduardoDev
  */
 public class socketCliente {
-    public static void main(String args[]){
+
+    public static void main(String args[]) {
+        ObjectInputStream entrada = null;
+        ObjectOutputStream salida = null;
+        String datos = "";
+        String respuesta="";
         try {
             System.out.println("Conectándose a servidor...");
             Socket servidor = new Socket("localhost", 12345);
-            ObjectOutputStream salida = new ObjectOutputStream(servidor.getOutputStream());
-            File archivo = new File("C:\\Users\\EduardoDev\\Desktop\\p.txt");
-            
-            
-            System.out.println("Enviando mensaje...");
-            salida.writeObject("EPALEEE");
-            System.out.println("Mensaje enviado!");
+            System.out.println("Leyendo archivo...");
+            salida = new ObjectOutputStream(servidor.getOutputStream());
+            datos = new String(Files.readAllBytes(Paths.get("op.json")));
+            salida.writeObject(datos);
+            System.out.println("Petición enviada!");
+            System.out.println("Esperando respuesta...");
+            entrada = new ObjectInputStream(servidor.getInputStream());
+            respuesta=(String)entrada.readObject();
+            System.out.println("Respuesta recibida: "+respuesta);
             salida.close();
             servidor.close();
         } catch (IOException ex) {
-            System.out.println("Error!: No se pudo establecer una conexion con el servidor especificado.");
+            ex.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(socketCliente.class.getName()).log(Level.SEVERE, null, ex);
         }
-        String data = ""; 
-    data = new String(Files.readAllBytes(Paths.get("RUta"))); 
-       
+
     }
 }
--- OperacionesJSON/src/server/socketServidor.java
+++ OperacionesJSON/src/server/socketServidor.java
@@ -7,6 +7,7 @@ package server;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.logging.Level;
@@ -18,21 +19,44 @@ import org.json.JSONObject;
  * @author EduardoDev
  */
 public class socketServidor {
-    public static void main(String args[]){
+
+    public static void main(String args[]) {
+        String datos = "";
+        String tipo = "";
+        ObjectInputStream entrada = null;
+        ObjectOutputStream salida = null;
+        JSONObject objetoJson = null;
+        float parametro1 = 0;
+        float parametro2 = 0;
+        float resultado = 0;
         try {
             ServerSocket servidor = new ServerSocket(12345);
             System.out.println("Esperando conexión...");
             Socket cliente = servidor.accept();
             System.out.println("Cliente conectado!");
-            ObjectInputStream entrada = new ObjectInputStream(cliente.getInputStream());
-            System.out.println("Leyendo mensaje...");
-            System.out.println((String)entrada.readObject());
-            
-            // JSON
-            JSONObject obj = new JSONObject("{\"name\": \"John\"}");
-
-System.out.println(obj.getString("name")); //John
-
+            entrada = new ObjectInputStream(cliente.getInputStream());
+            System.out.println("Leyendo petición...");
+            datos = (String) entrada.readObject();
+            objetoJson = new JSONObject(datos);
+            tipo = objetoJson.getString("tipo");
+            parametro1 = objetoJson.getFloat("param1");
+            parametro2 = objetoJson.getFloat("param2");
+            switch (tipo) {
+                case "+":
+                    resultado = parametro1 + parametro2;
+                    break;
+                case "-":
+                    resultado = parametro1 - parametro2;
+                    break;
+                case "*":
+                    resultado = parametro1 * parametro2;
+                    break;
+                case "/":
+                    resultado = parametro1 / parametro2;
+            }
+            System.out.println("Enviando respuesta...");
+            salida = new ObjectOutputStream(cliente.getOutputStream());
+            salida.writeObject(resultado + "");
         } catch (IOException ex) {
             System.out.println("Error!: No se pudo inicializar el servidor.");
         } catch (ClassNotFoundException ex) {
