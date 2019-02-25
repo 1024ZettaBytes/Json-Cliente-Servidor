4b457d5de5ff7b5fcd53ff8a2aa83eed3fa9980a
Eduardo Ramírez <ramirezcota.eduardo@gmail.com>
2019-02-25 09:39:57 -0700

    Final

--- OperacionesJSON/src/client/socketCliente.java
+++ OperacionesJSON/src/client/socketCliente.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package client;
 
 import java.io.File;
@@ -20,7 +16,7 @@ import org.json.JSONObject;
 
 /**
  *
- * @author EduardoDev
+ * @author Eduardo
  */
 public class socketCliente {
 
--- OperacionesJSON/src/server/socketServidor.java
+++ OperacionesJSON/src/server/socketServidor.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package server;
 
 import java.io.IOException;
@@ -16,7 +12,7 @@ import org.json.JSONObject;
 
 /**
  *
- * @author EduardoDev
+ * @author Eduardo
  */
 public class socketServidor {
 
